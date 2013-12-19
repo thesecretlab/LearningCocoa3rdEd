306c92ad5fea15731a690be51e08914103197882
Tim Nugent <timrnugent@gmail.com>
2013-12-19 12:39:29 +1100

    DataDetectors: fixed copyright

--- DataDetectors/DataDetectors/AppDelegate.h
+++ DataDetectors/DataDetectors/AppDelegate.h
@@ -2,8 +2,8 @@
 //  AppDelegate.h
 //  DataDetectors
 //
-//  Created by Jon Manning on 10/04/12.
-//  Copyright (c) 2012 Secret Lab. All rights reserved.
+//	Created for “Learning Cocoa with Objective-C Third Edition”
+//	Copyright (c) 2012-2013 the authors of the book. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
