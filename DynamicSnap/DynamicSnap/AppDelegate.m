316f73052869329081f6e781c15d65c699d3c8b4
Tim Nugent <timrnugent@gmail.com>
2013-12-19 12:43:03 +1100

    DynamicSnap: fixed copyright

--- DynamicSnap/DynamicSnap/AppDelegate.m
+++ DynamicSnap/DynamicSnap/AppDelegate.m
@@ -2,8 +2,8 @@
 //  AppDelegate.m
 //  DynamicSnap
 //
-//  Created by Tim Nugent on 6/11/2013.
-//  Copyright (c) 2013 Tim Nugent. All rights reserved.
+//	Created for “Learning Cocoa with Objective-C Third Edition”
+//	Copyright (c) 2012-2013 the authors of the book. All rights reserved.
 //
 
 #import "AppDelegate.h"
