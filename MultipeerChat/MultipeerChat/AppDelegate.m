054bf02399ea2a11b70ffd982ce7fe068dd93f58
Tim Nugent <timrnugent@gmail.com>
2013-12-19 13:27:03 +1100

    Multipeer: fixed copyright

--- MultipeerChat/MultipeerChat/AppDelegate.m
+++ MultipeerChat/MultipeerChat/AppDelegate.m
@@ -2,8 +2,8 @@
 //  AppDelegate.m
 //  MultipeerChat
 //
-//  Created by Tim Nugent on 15/10/2013.
-//  Copyright (c) 2013 Tim Nugent. All rights reserved.
+//	Created for “Learning Cocoa with Objective-C Third Edition”
+//	Copyright (c) 2012-2013 the authors of the book. All rights reserved.
 //
 
 #import "AppDelegate.h"
