6787c84a2eb34b417a7ee9f1e0b41ce3cfed737b
Tim Nugent <timrnugent@gmail.com>
2013-12-19 12:51:46 +1100

    EventKit: fixed copyright, made it match book

--- EventKit/EventKit/AppDelegate.m
+++ EventKit/EventKit/AppDelegate.m
@@ -2,8 +2,8 @@
 //  AppDelegate.m
 //  EventKit
 //
-//  Created by Jon Manning on 16/07/12.
-//  Copyright (c) 2012 Secret Lab. All rights reserved.
+//	Created for “Learning Cocoa with Objective-C Third Edition”
+//	Copyright (c) 2012-2013 the authors of the book. All rights reserved.
 //
 
 #import "AppDelegate.h"
@@ -23,6 +23,8 @@
 @implementation AppDelegate
 @dynamic calendars;
 @dynamic events;
+@synthesize date = _date;
+@synthesize store = _store;
 
 - (NSDate *)date {
     if (_date == nil) {
--- EventKit/EventKit/main.m
+++ EventKit/EventKit/main.m
@@ -2,8 +2,8 @@
 //  main.m
 //  EventKit
 //
-//  Created by Jon Manning on 16/07/12.
-//  Copyright (c) 2012 Secret Lab. All rights reserved.
+//	Created for “Learning Cocoa with Objective-C Third Edition”
+//	Copyright (c) 2012-2013 the authors of the book. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
